a723a2a9e694c63378ac9bc19b8579a4d8c0cb2a
Christopher Barnes <cbarnes7404@yahoo.com>
2019-09-26 16:48:57 -0700

    init

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    return render_template('home.html', msg='Flask is Cool!!')
