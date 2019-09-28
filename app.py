c9a5a6bd3a902b2b71130a9eb59671c78f7c78d7
Christopher Barnes <cbarnes7404@yahoo.com>
2019-09-28 12:42:26 -0700

    USers can eidt, update, and destroy playlists

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ app = Flask(__name__)
     {'title': 'Rock Music', 'description': 'I love rock and roll'}
 ]"""
 
+# INDEX
 @app.route('/')
 def playlists_index():
     """Return homepage."""
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
 
+# CREATE NEW
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', title='New Playlist')
 
+# DISPLAY/SHOW
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
     """Submit a new playlist."""
@@ -35,14 +38,39 @@ def playlists_submit():
         'videos': request.form.get('videos').split(),
         'ratings': request.form.get('ratings')
     }
-    playlist_id = playlists.insert_one(playlist).insertedd_id
+    playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+# DISPLAY/SHOW FROM ID
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
+
+# EDIT
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    video_links = '\n'.join(playlist.get('videos'))
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+# UPDATE
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
